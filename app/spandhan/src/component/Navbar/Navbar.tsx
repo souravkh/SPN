fd40fe1bb1d107b1ca8558c96e7cb558527d7080
souravkh <souravkhatri1996@gmail.com>
2026-06-14 23:11:48 +0530

    structured navbar

--- app/spandhan/src/component/Navbar/Navbar.tsx
+++ app/spandhan/src/component/Navbar/Navbar.tsx
@@ -3,27 +3,27 @@ import { useState } from "react";
 
 
 function Navbar(){
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState<Boolean>(false);
     return (
         <>
         <div className='navbar'>
             <div className='logo'>
                 <h4>
-                    SPANDHAN
+                    SPANDAN
                 </h4>
             </div>
 
            <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-        ☰
-      </div>   
-         <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
-            <li><a href="#">Home</a></li>
-  <li><a href="#">About</a></li>
-  <li><a href="#">Milestone</a></li>
-  <li><a href="#">Testimonial</a></li>
-  <li><a href="#">Team</a></li>
-  <li><a href="#">Volunteer</a></li>
-         </ul>   
+            ☰
+            </div>   
+            <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Milestone</a></li>
+                <li><a href="#">Testimonial</a></li>
+                <li><a href="#">Team</a></li>
+                <li><a href="#">Volunteer</a></li>
+            </ul>   
         </div>
         </>
     )
